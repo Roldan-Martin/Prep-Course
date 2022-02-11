06f0f8bdfe2ca2317a5c99b24c6c001fa7ff37d2
Martín <martineliasroldan@gmail.com>
2022-02-11 19:19:09 -0300

    primer trabajo terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Soy Tincho';
+const nuevaString = 'Quiero ser un Henry';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 18;
@@ -67,7 +67,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return(str1.length == str2.length)
+  return(str1.length == str2.length);
 }
 
 function menosQueNoventa(num) {
